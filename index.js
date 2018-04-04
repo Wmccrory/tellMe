fd7f78a02757bccbc8c422be08cb7e70cfe15068
Will <wmccrory@gmail.com>
2018-04-03 19:58:44 -0500

    Preparing for heroku deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //dependencies
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send({ hi: 'there'});
