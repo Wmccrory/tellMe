fee37a59bcf000b4e68da30f9244712a82e908ec
Will <wmccrory@gmail.com>
2018-04-04 21:50:47 -0500

    google authentication working. refactoring app

--- index.js
+++ index.js
@@ -4,11 +4,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+require('./services/passport');
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+//routes
 
+require('./routes/authRoutes')(app);	//authentication
+
+//starting up server
 app.listen(PORT, () => {
-    console.log(`Now listening on port ${PORT}`);
+	console.log(`Now listening on port ${PORT}`);
 });
+
+//notes
+//
+//* authentication routes are in routers/authRoutes.js
+//* passport app (for authenticating) in services/passport.js
---
+++ routes/authRoutes.js
@@ -0,0 +1,18 @@
+//authentication routes//
+
+//dependencies
+const passport = require('passport');
+
+////////////////////
+
+module.exports = (app) => {
+
+    //google authentication
+
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+
+    app.get('/auth/google/callback', passport.authenticate('google'));
+};
+
