4c3269330373c3e071d9c59e8a05220573431411
Will <wmccrory@gmail.com>
2018-04-05 01:29:36 -0500

    Removing gitignore item for heroku deployment

---
+++ config/keys.js
@@ -0,0 +1,15 @@
+module.exports = {
+    //google info
+    googleClientID: "111583513904-cnqq5ukb3p3vpfqpuq6cvqpvlgl21dam.apps.googleusercontent.com",
+    googleClientSecret: "PEdtbSWwUXNCm3zuSjJnIVpt",
+
+    //facebook info
+    facebookClientID: "194696547805276",
+    facebookClientSecret: "a9d1e8df1ca643f3956f80fc37276a79",
+
+    //mongo info
+    mongoURI: "mongodb://tellmedev:tellmedev@ds235239.mlab.com:35239/tellme-dev",
+
+    //cookie info
+    cookieKey: "asdfoijwpiencnxoucoiqjqwpoirkladfkjcpakqpwdio"
+};
