acdb7dc25b6c9010381b93848781140fb90788e9
Will <wmccrory@gmail.com>
2018-04-08 00:00:05 -0500

    Added heroku build commands

--- index.js
+++ index.js
@@ -34,6 +34,15 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);	//user authentication
 require('./routes/billingRoutes')(app);	//billing routes
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 //starting up server
 app.listen(PORT, () => {
 	console.log(`Now listening on port ${PORT}`);
