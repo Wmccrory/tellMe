2698bcee7a35edf92883b6e4db260ee5d89efffd
Will <wmccrory@gmail.com>
2018-04-09 13:32:37 -0500

    Email implementation functioning on frontend

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+	{label: 'Survey Title (For internal use)', name: 'title'},
+	{label: 'Email Subject Line', name: 'subject'},
+	{label: 'Email Body', name: 'body'},
+	{label: 'Recipient List', name: 'recipients'},
+];
